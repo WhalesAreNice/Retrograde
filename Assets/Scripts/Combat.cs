7d3593fa1771c2c943c37f3ab3bf307c482fc626
Allan Ng (RIT Student) <an2154@ad.rit.edu>
2020-03-03 17:10:05 -0500

    scripts

---
+++ Assets/Scripts/Combat.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat : MonoBehaviour
+{
+    //player's variables
+    public GameObject player;
+    Player playerScript;
+    string diceType;
+    int diceSides;
+    List<int> diceValues;   //list of dice face values
+
+    //enemy's variables
+    public GameObject manager;
+    Manager managerScript;
+
+    //using placeholder enemy object for now
+    public GameObject enemy;
+    Enemy enemyScript;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript = player.GetComponent<Player>();
+        //diceType = playerScript.diceType;
+        //diceSides = playerScript.diceSides;
+        diceValues = playerScript.diceValues;
+
+        enemyScript = enemy.GetComponent<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //placeholder for now to test code
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            DealDamage(); //player deals damage first
+            playerScript.TakeDamage(enemyScript.DealDamage()); //takes damage from enemy 
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            MakeShield(); //create a shield for the player
+            playerScript.TakeDamage(enemyScript.DealDamage()); //takes damage from enemy 
+        }
+
+        Debug.Log("Enemy Health : " + enemyScript.health +
+            "\nPlayer Health: " + playerScript.health + " Player Shield: " + playerScript.shield);
+    }
+
+    int RollDiceIndex()
+    {
+        return Random.Range(0, 6); //returns the index between 0 to amount of dice sides
+    }
+
+    void MakeShield()
+    {
+        playerScript.shield += diceValues[RollDiceIndex()];
+    }
+
+    void DealDamage()
+    {
+        enemyScript.TakeDamage(diceValues[RollDiceIndex()]);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health;
+    public int shield;
+    public List<int> diceValues;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 10;
+        shield = 0;
+        for(int i = 1; i <= 3; i++)
+        {
+            diceValues.Add(i);
+            diceValues.Add(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        //if there is a shield 
+        if(shield > 0)
+        {
+            //if the shield is more than the incoming damage
+            if(shield >= damageAmount)
+            {
+                shield -= damageAmount;
+            }
+            else //if sheild isn't, then take damage on shield first and the rest onto health
+            {
+                int damageLeft = damageAmount - shield;
+                health -= damageLeft;
+            }
+        }
+        else //no shield available so take damage on health
+        {
+            health -= damageAmount;
+        }
+    }
+}
