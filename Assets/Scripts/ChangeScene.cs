46b9f639ea08d02dfad0766107ecf7f4eca2bafe
axs7249 <axs7249@g.rit.edu>
2020-04-11 19:34:49 -0400

    created changescene

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    private Player playerScript;
+    private Enemy enemyScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript = GameObject.Find("Player").GetComponent<Player>();
+        enemyScript = GameObject.Find("Enemy").GetComponent<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerScript.health <= 0 || enemyScript.health <= 0)
+        {
+            playerScript.Reset();
+            enemyScript.Reset();
+        }
+    }
+}
