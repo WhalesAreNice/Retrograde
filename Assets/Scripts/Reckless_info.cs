56bcb7e8099c16a913303ead4e6bc702f57e6241
swagpenny <swagpenny000@gmail.com>
2020-04-20 08:38:16 -0400

    Enemy Intention UI fix and more little changes

---
+++ Assets/Scripts/Reckless_info.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reckless_info : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnMouseOver();
+        OnMouseExit();
+    }
+
+
+    void OnMouseOver()
+    {
+        Debug.Log("Mouse is over");
+    }
+
+    void OnMouseExit()
+    {
+        Debug.Log("no over");
+    }
+}
